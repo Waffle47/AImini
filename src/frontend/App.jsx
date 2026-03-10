83dc081d2d76a69646804a69484e1a86ed65f2a7
Waffle47 <wafulanelson2004@gmail.com>
2026-03-10 11:53:59 +0300

    gh pages

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -119,7 +119,7 @@ function DetectorTab() {
     try {
       const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-      const res = await fetch(`${API_URL}/api/analyse`, {
+      const res = await fetch(`https://aimini.onrender.com`, {
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body: JSON.stringify({ email })
